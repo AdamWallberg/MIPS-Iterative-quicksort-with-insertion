7bf70a983eadd80d36793611d5a18b556074db0e
Malakor <viking_metus2@hotmail.com>
2018-05-03 16:36:44 +0200

    Add zero functionality

--- sorting.asm
+++ sorting.asm
@@ -1,23 +1,13 @@
 	.data
 datalen:
-	.word	0x0010	# 16
+	.word	0x0000	# 16
 data:
-	.word	0xffff7e81
-	.word	0x00000001
-	.word	0x00000002
-	.word	0xffff0001
-	.word	0x00000000
-	.word	0x00000001
-	.word	0xffffffff
-	.word	0x00000000
-	.word	0xe3456687
-	.word	0xa001aa88
-	.word	0xf0e159ea
-	.word	0x9152137b
-	.word	0xaab385a1
-	.word	0x31093c54
-	.word	0x42102f37
-	.word	0x00ee655b
+
+
+.globl datalen
+.globl data
+
+	
 # Insert data here
 
 newline:
@@ -35,6 +25,13 @@ main:
 # been inlined, and conventions relating to registers have not
 # been followed.
 
+	#Quit if datalength is 0
+	lw $t0, datalen
+	bnez $t0, notzero
+	addi $v0, $v0, 10
+	syscall
+	notzero:
+	
 	la		$s0, data 	# Data pointer
 	li 		$s1, 0		# Low
 	lw 		$s2, datalen	# High
