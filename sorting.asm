df5dbce0e82a7c0f817ca286c7ac14dfb3ebd00e
Malakor <viking_metus2@hotmail.com>
2018-05-03 16:02:50 +0200

    Fix print bug

--- sorting.asm
+++ sorting.asm
@@ -1,5 +1,23 @@
 	.data
-	
+datalen:
+	.word	0x0010	# 16
+data:
+	.word	0xffff7e81
+	.word	0x00000001
+	.word	0x00000002
+	.word	0xffff0001
+	.word	0x00000000
+	.word	0x00000001
+	.word	0xffffffff
+	.word	0x00000000
+	.word	0xe3456687
+	.word	0xa001aa88
+	.word	0xf0e159ea
+	.word	0x9152137b
+	.word	0xaab385a1
+	.word	0x31093c54
+	.word	0x42102f37
+	.word	0x00ee655b
 # Insert data here
 
 newline:
@@ -228,7 +246,6 @@ main:
 	
 	move 	$t0, $s0
 	addi	$t0, $t0, 4
-	addi	$t4, $t4, -4
 	move 	$t1, $t4
 	add 	$t1, $t1, $s0
 	L5:
@@ -241,7 +258,7 @@ main:
 		syscall
 		
 		addi	$t0, $t0, 4
-		blt  	$t0, $t1, L5
+		ble  	$t0, $t1, L5
 
 	### End printing
 
